c0a9216281d5cbdee64326155df5dcb62bd70299
Riley <dickersonevan82@gmail.com>
2024-11-07 18:32:51 -0500

    moved index.css to new styles folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '/styles/index.css'
 import SideContent from './SideContent.jsx'
 import MainContent from './MainContent.jsx'
 
