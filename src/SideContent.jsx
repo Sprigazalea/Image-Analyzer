05f99632b98dafbe013a6528641c5dbacd281b45
Riley <dickersonevan82@gmail.com>
2024-11-07 19:10:07 -0500

    added fontawesome through npm and added arrow icon

--- src/SideContent.jsx
+++ src/SideContent.jsx
@@ -1,11 +1,13 @@
 import '/styles/index.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDownLong } from '@fortawesome/free-solid-svg-icons'
 
 function SideContent() {
     return (
         <>
         <div className="default-side-div">
             <div className='inner-div'>
-                <div className='drag-div'></div>
+                <div className='drag-div'><FontAwesomeIcon icon={faDownLong} /></div>
             </div>
         </div>
         </>
