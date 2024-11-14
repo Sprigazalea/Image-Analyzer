ed52728914879813e76066dcec295804662fd829
Riley <dickersonevan82@gmail.com>
2024-11-14 01:55:39 -0500

    experimenting with placeholder

--- src/Test.jsx
+++ src/Test.jsx
@@ -3,7 +3,7 @@ import '/styles/index.css';
 export default function test() {
     return (
         <>
-            <textarea placeholder='&lt;----- Insert Image Over There!'></textarea>
+            <textarea placeholder='&lt;----- drag image into this box!'></textarea>
         </>
     )
 }
