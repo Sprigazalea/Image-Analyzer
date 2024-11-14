a18ac2762ce5230624cb0c89a00ca94b579d31f0
Riley <dickersonevan82@gmail.com>
2024-11-13 23:39:11 -0500

    added api key, not working tho :(

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,5 +1,12 @@
+import { Editor } from '@tinymce/tinymce-react';
 import '/styles/index.css';
 
 export default function test() {
-
-    }
+    return (
+        <>
+            <Editor
+                apiKey='ung154lpbx6rzgualpycfxkwfmv0x1m0i9cp3urgl6e1jjfe'
+            />
+        </>
+    )
+}
