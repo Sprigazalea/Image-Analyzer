36a0d3668742bd34c5bffb966bea8e818c7cb42e
Riley <dickersonevan82@gmail.com>
2024-11-14 00:27:21 -0500

    going back to textarea :(

--- src/Test.jsx
+++ src/Test.jsx
@@ -4,9 +4,7 @@ import '/styles/index.css';
 export default function test() {
     return (
         <>
-            <Editor
-                apiKey='ung154lpbx6rzgualpycfxkwfmv0x1m0i9cp3urgl6e1jjfe'
-            />
+            <textarea></textarea>
         </>
     )
 }
