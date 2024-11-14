0c956925556373962830da37a5a298ea985107d5
Riley <dickersonevan82@gmail.com>
2024-11-13 23:31:59 -0500

    removed blocknote implementation

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,13 +1,5 @@
-import "@blocknote/core/fonts/inter.css";
-import { BlockNoteView } from "@blocknote/mantine";
-import "@blocknote/mantine/style.css";
-import { useCreateBlockNote } from "@blocknote/react";
 import '/styles/index.css';
 
 export default function test() {
-    // Creates a new editor instance.
-    const editor = useCreateBlockNote();
-    
-    // Renders the editor instance using a React component.
-    return <BlockNoteView editor={editor} data-theming-css-variables />;
+
     }
