0a5d3c41e708962b98a9d2d085d2e162c851b1fe
Riley <dickersonevan82@gmail.com>
2025-01-13 16:50:42 -0500

    removed temporary comments

--- src/ParseImage.jsx
+++ src/ParseImage.jsx
@@ -7,14 +7,6 @@ export default function ParseImage(imageFile) {
             const reader = new FileReader();
             const rawOutput = JSON.stringify(value, null, 1);
 
-            //console.log(imageFile)
-            //console.log(imageFile.name)
-            //console.log(imageFile.lastModified)
-            //console.log(imageFile.webkitRelativePath)
-            //console.log(imageFile.size)
-            //console.log(imageFile.type)
-            //console.log(JSON.stringify(value))
-
             const formattedString = rawOutput
 
             console.log(formattedString)
@@ -27,14 +19,3 @@ export default function ParseImage(imageFile) {
         Output()
     )
 }
-
-//might need to research how to organize react files in a file system
-
-// 1. turn rawOutput into a string
-// 2. use split and join to add line breaks
-// 3. add {} at beginning and end with line breaks to replicate json look
-// or
-// 1. turn rawOutput into a string
-// 2. use str.replace()
-
-// str.replace seems to make the most sense
