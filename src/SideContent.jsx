ea61b6440a6b0cd49bff52cd195450efa192ebb5
Riley <dickersonevan82@gmail.com>
2025-01-22 16:34:03 -0500

    added imageFile as an argument for RemoveExif

--- src/SideContent.jsx
+++ src/SideContent.jsx
@@ -114,7 +114,7 @@ function SideContent() {
             <div className='side-options'>
                 <a onClick={() => {setIsFAQOpen(true); toggleDim()}}><p>FAQ</p><FontAwesomeIcon icon={faAngleRight} className='right-icon'/></a>
                 <a onClick={() => {setIsCompatOpen(true); toggleDim()}}><p>Compatibility</p><FontAwesomeIcon icon={faAngleRight} className='right-icon'/></a>
-                <a onClick={() => {NoImage(); RemoveEXIF()}}><p>Remove EXIF Data</p> <FontAwesomeIcon icon={faAngleRight} className='right-icon'/></a>
+                <a onClick={() => {NoImage(); RemoveEXIF(imageFile)}}><p>Remove EXIF Data</p> <FontAwesomeIcon icon={faAngleRight} className='right-icon'/></a>
                 <a onClick={() => {NoImage()}}><p>Export</p> <FontAwesomeIcon icon={faAngleRight} className='right-icon'/></a>
                 <a onClick={() => {setIsCreditsOpen(true); toggleDim()}}><p>Credits</p><FontAwesomeIcon icon={faAngleRight} className='right-icon'/></a>
             </div>
